e0d4647c9ad46a81344eb041a58ce84ea10b4cbc
Unknown <jazqwolf14@gmail.com>
2018-09-26 12:08:37 -0600

    My first playground (Foot-Bar)

--- src/modules/main-page/playground/Playground.js
+++ src/modules/main-page/playground/Playground.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './Playground.css';
-import ItemMenu from '../components/item-menu/ItemMenu';
+import FootBar from '../components/foot-bar/FootBar';
 export default class Playground extends Component {
   render() {
-    return <ItemMenu />;
+    return <FootBar />;
   }
 }
