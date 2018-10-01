b0812b2723b38bd1469f00aef6f0ed1d1d550879
Unknown <jazqwolf14@gmail.com>
2018-09-30 18:28:13 -0600

    Foot-bar

--- src/modules/main-page/components/foot-bar/FootBar.js
+++ src/modules/main-page/components/foot-bar/FootBar.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './FootBar.css';
 export default class FootBar extends Component {
   render() {
-    return <div className="component-FootBar" />;
+    return(
+    <div className="component-FootBar" />,
+    <div>     
+        <hr className= "hr"/>,
+        <button class="button">
+         <h1 className="h1">i</h1> 
+        </button>
+     </div>
+    )
+  
   }
 }
