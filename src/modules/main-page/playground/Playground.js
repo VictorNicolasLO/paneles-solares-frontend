6329f6dacb321d4aa538838209ab717494b5a735
VictorNicolasLO <victornicolaslo@hotmail.com>
2018-10-01 14:39:40 -0600

    Small changes

--- src/modules/main-page/playground/Playground.js
+++ src/modules/main-page/playground/Playground.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import './Playground.css';
 import FootBar from '../components/foot-bar/FootBar';
 export default class Playground extends Component {
+  clickTest = () => {
+    alert('Hey');
+  };
+
   render() {
-    return <FootBar />;
+    return <FootBar onShowInfo={this.clickTest} />;
   }
 }
