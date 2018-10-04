b86471d5ece3870fd4bf4f7c7b35d4cc292441d5
VictorNicolasLO <victornicolaslo@hotmail.com>
2018-10-04 11:59:39 -0600

    terminado

--- src/modules/main-page/components/item-menu/ItemMenu.js
+++ src/modules/main-page/components/item-menu/ItemMenu.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './ItemMenu.css';
+
 export default class ItemMenu extends Component {
   render() {
+    const { name, Icon } = this.props;
+
     return (
       <div className="item-menu">
-        <div className="icon">I</div>
-        <div className="name">Example asd</div>
+        <div className="icon">
+          <Icon className="item-menu-icon" />
+        </div>
+        <div className="name">{name}</div>
       </div>
     );
   }
