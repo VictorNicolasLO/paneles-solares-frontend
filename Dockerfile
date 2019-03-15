969593da069d925badd357ef8b388fde7116a550
VictorNicolasLO <victornicolaslo@hotmail.com>
2019-03-15 13:22:34 -0700

    m

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN npm cache clean --force
 RUN npm install serve -g
+WORKDIR /usr/src/app/build
 EXPOSE 5000
-CMD ["serve", "-s build"]
+CMD ["serve", "-s"]
---
+++ build/precache-manifest.914889134e491446320b610b2fd0d4cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b30bbbbae5e78df75b0",
+    "url": "/static/js/main.0b30bbbb.chunk.js"
+  },
+  {
+    "revision": "39ff7d202a32d75b3188",
+    "url": "/static/js/1.39ff7d20.chunk.js"
+  },
+  {
+    "revision": "0b30bbbbae5e78df75b0",
+    "url": "/static/css/main.c0f45e96.chunk.css"
+  },
+  {
+    "revision": "b9fe413acd74d8f8868eb67cecbfc0f3",
+    "url": "/index.html"
+  }
+];
